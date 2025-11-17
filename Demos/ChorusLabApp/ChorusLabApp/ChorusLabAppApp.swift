bb87b75eba21999a4780d8ace028d42ecf6ae75a
Robert Doggett <robert@peacedudes.com>
2025-11-17 07:15:52 -0800

    clone chorusLab into app checkpoint

---
+++ Demos/ChorusLabApp/ChorusLabApp/ChorusLabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChorusLabAppApp.swift
+//  ChorusLabApp
+//
+//  Created by robert on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ChorusLabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demos/ChorusLabApp/ChorusLabAppTests/ChorusLabAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChorusLabAppTests.swift
+//  ChorusLabAppTests
+//
+//  Created by robert on 11/17/25.
+//
+
+import Testing
+
+struct ChorusLabAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
